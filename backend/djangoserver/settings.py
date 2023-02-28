839d4fe47db252cbaaf012bd6377cdf729eb2fc0
NJ Nafir <njnafir@gmail.com>
2023-02-28 15:59:05 +0600

    Change database to postgres

--- backend/djangoserver/settings.py
+++ backend/djangoserver/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp'
+    'testapp',
+    'chat'
 ]
 
 MIDDLEWARE = [
@@ -70,17 +71,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoserver.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'heaven',
+        'USER': 'localdjangoprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
